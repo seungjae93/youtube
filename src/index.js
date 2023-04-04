892059476ac3558534bb2fb62a6df7bf2e46d0a8
seungjae93 <lsj72023892@gmail.com>
2023-04-04 18:42:16 +0900

    :sparkles: [feat] router 설정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import NotFound from "./pages/NotFound";
+import Videos from "./pages/Videos";
+import VideoDtail from "./pages/VideoDtail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: "videos", element: <Videos /> },
+      { path: "videos/:keyword", element: <Videos /> },
+      { path: "videos/watch/:videoId", element: <VideoDtail /> },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/VideoDtail.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function VideoDtail() {
+  return <div>VideoDtail</div>;
+}
