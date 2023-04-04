fa3276316c5c8ce2da953ba582f51b5c28e1cc4f
seungjae93 <lsj72023892@gmail.com>
2023-04-04 19:42:47 +0900

    :lipstick: [feat] SearchHeader tailwind 스타일링

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
 export default function Videos() {
-  return <div>Videos</div>;
+  const { keyword } = useParams();
+  return <div>Videos {keyword ? `😀${keyword}` : "😊"}</div>;
 }
