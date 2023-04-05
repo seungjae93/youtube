7976e61d5c525750386ab88d3d27d408e98572de
seungjae93 <lsj72023892@gmail.com>
2023-04-05 22:59:07 +0900

    :sparkles: [feat] 더미데이터 리스트 만들기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,6 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
+import FakeYoutube from "../api/fakeYoutube";
+
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `😀${keyword}` : "😊"}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], () => {
+    const youtube = new FakeYoutube();
+    return youtube.search(keyword);
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `😀${keyword}` : "😊"}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong...</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
