761049bc80f8cd026e17b7985800b8bbf65aa45c
seungjae93 <lsj72023892@gmail.com>
2023-04-06 15:07:23 +0900

    :sparkles: [feat] Context 사용

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,18 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
 import VideoCard from "../components/VideoCard";
-import FakeYoutube from "../api/fakeYoutube";
+import FakeYoutube from "../api/fakeYoutubeClient";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => {
-    const youtube = new FakeYoutube();
-    return youtube.search(keyword);
-  });
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
   return (
     <>
       <div>Videos {keyword ? `😀${keyword}` : "😊"}</div>
